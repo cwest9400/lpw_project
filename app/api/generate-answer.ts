9e0a0a32da695df2d9dd5fb04d1a1f282adacc64
cwest9400 <chrstphrwest@gmail.com>
2023-03-09 11:48:20 -0500

    api connection

--- app/api/generate-answer.ts
+++ app/api/generate-answer.ts
@@ -1,3 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Configuration, OpenAIApi } from 'openai';
 
+type ResponseData = {
+    text: string;
+};
+
+interface GenerateNextApiRequest extends NextApiRequest {
+    body: {
+        prompt: string;
+    };
+};
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+    req: GenerateNextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    
+}
