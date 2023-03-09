a608a76ff176414e3b748b51193944f27f38e74d
cwest9400 <chrstphrwest@gmail.com>
2023-03-09 14:30:00 -0500

    chat

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import Image from 'next/image'
 import useState from 'react-usestateref'
-import userPic from'../public/user.webp';
-import botPic from '../public/bot.png';
+// import userPic from'../public/user.webp';
+// import botPic from '../public/bot.png';
 import styles from './page.module.css';
 
 enum Creator {
@@ -21,6 +21,23 @@ interface InputProps {
   disabled: boolean;
 }
 
+const ChatMessage = ({ text, from}: MessageProps) => {
+  return (
+    <>
+    {from == Creator.Me && (
+      <div className='bg-white p-4 rounded-lg flex gap-4 items-center whitespace-pre-wrap'>
+        {/* <Image src={userPic} alt="User" width={40} /> */}
+        <p className='text-gray-700'>{text}</p>
+      </div>
+    )}
+    {from == Creator.Bot && (
+      <div className='bg-gray-100 p-4 rounded-lg flex gap-4 items-center whitespace-pre-wrap'>
+        <p className='text-gray-700'>{text}</p>
+      </div>
+    )}
+    </>
+  )
+}
 
 export default function Home() {
   return (
